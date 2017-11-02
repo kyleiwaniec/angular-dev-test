26edaec9594cb99d2d6ed2d9e025f02a53603034
Scott Kennedy <scottyzen@gmail.com>
2017-11-02 12:32:29 +0000

    Checknox and Search
    Create a checkbox that toggles all users selection.
    
    Fixed the search filter so that when a user starts typing into the
    search field, members with names starting with the substring will be
    displayed.
    
    Selected letter highlighting

--- boilerplate/app/scripts/controllers/filterbar.js
+++ boilerplate/app/scripts/controllers/filterbar.js
@@ -7,8 +7,23 @@
  * # FilterbarCtrl
  * Controller of the angular14InterviewApp
  */
-angular.module('angular14InterviewApp')
-  .controller('FilterbarCtrl', ['$scope', 'filterbarService', '$http', function ($scope, filterbarService, $http) {
+angular.module("angular14InterviewApp").service("serve", function() {
+  this.memberObj = [];
+});
+
+angular.module("angular14InterviewApp").controller("FilterbarCtrl", [ "$scope", "serve", "filterbarService", "$http", function($scope, serve, filterbarService, $http) {
+    $scope.$watch(
+      function() {
+        return serve.memberObj;
+      },
+      function(newData, oldData) {
+        $scope.selectedMembers = newData;
+        if (newData !== oldData) {
+          $scope.selectedMembers = newData;
+        }
+      }
+    );
+
     $scope.alphabeth = filterbarService.alphabeth;
     $scope.selectedLetterIndex = null;
     $scope.selectLetter = function(index) {
@@ -22,22 +37,31 @@ angular.module('angular14InterviewApp')
       $scope.quantity += onLoadQuantity;
     };
 
+    filterbarService.getMembers().then(function(data) {
+      $scope.members = data;
+    });
 
-
-   filterbarService.getMembers()
-     .then(function(data) {
-       $scope.members = data;
-     })
-
-    $scope.selectedMembers = [];
+    // $scope.selectedMembers = [];
     $scope.toggleSelectedMember = function(index) {
       var isSelected = $scope.selectedMembers.indexOf(index);
-      if(isSelected === -1) return $scope.selectedMembers.push(index);
-
+      if (isSelected === -1) return $scope.selectedMembers.push(index);
       $scope.selectedMembers.splice(isSelected, 1);
-      console.log($scope.selectedMembers);
-      debugger
     };
 
+    $scope.checkboxStyle = false;
 
-  }]);
+    $scope.toggleAllMembers = function() {
+      if ($scope.selectedMembers.length < $scope.quantity) {
+        for (var i = 0; i < $scope.quantity; i++) {
+          $scope.selectedMembers.push(i);
+        }
+      } else {
+        for (var i = $scope.selectedMembers.length; i >= 0; i--) {
+          $scope.selectedMembers.splice(i, 1);
+        }
+      }
+      var x = false;
+      $scope.checkboxStyle = !$scope.checkboxStyle;
+    };
+  }
+]);
--- boilerplate/app/scripts/filters/firstletterfilter.js
+++ boilerplate/app/scripts/filters/firstletterfilter.js
@@ -8,18 +8,29 @@
  * # firstLetterFilter
  * Filter in the angular14InterviewApp.
  */
-angular.module('angular14InterviewApp')
-  .filter('firstLetterFilter', function () {
-    return function (input, startWithLetter) {
-      console.log(input);
-      console.log(startWithLetter);
-      if(!startWithLetter) return input;
-      var filtered = [];
-      angular.forEach(input, function(item) {
-        if(startWithLetter.toLowerCase() === item.nm.charAt(0).toLowerCase()) {
-          filtered.push(item);
-        }
-      });
-      return filtered;
-    };
-  });
+angular.module("angular14InterviewApp").filter("firstLetterFilter", function() {
+  return function(input, startWithLetter) {
+    if (!startWithLetter) return input;
+    var filtered = [];
+    angular.forEach(input, item => {
+      if (startWithLetter.toLowerCase() === item.nm.charAt(0).toLowerCase()) {
+        filtered.push(item);
+      }
+    });
+    return filtered;
+  };
+});
+
+angular.module("angular14InterviewApp").filter("mySearch", function() {
+  return function(input, text) {
+    if (text === undefined || text === "") return input;
+
+    var filtered = [];
+    angular.forEach(input, item => {
+      if (item.nm.toLowerCase().indexOf(text.toLowerCase()) == 0) {
+        filtered.push(item);
+      }
+    });
+    return filtered;
+  };
+});
